3346158d058b1919efd7a3fb2d5c93c20b0cdb1a
Sirawit Yuwasirinun <nobsirawit@gmail.com>
2020-10-26 18:01:32 +0700

    validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -28,6 +28,29 @@ class PostController extends Controller
         return view('posts.show',['post' => BlogPost::findOrFail($id)]);
     }
 
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title'   => 'bail|required|min:5|max:100' ,
+            'content' => 'bail|required|min:10'
+        ]);
+
+        $blogPost = new BlogPost();
+        $blogPost->title = $request->input('title');
+        $blogPost->content = $request->input('content');
+        $blogPost->save();
+
+        $request->session()->flash('status','Blog post was created!');
+
+        //return redirect()->route('posts.index');
+        return redirect()->route('posts.show', ['post' => $blogPost -> id]);
+    }
+
     
     
 
